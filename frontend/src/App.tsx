5b14fef970e59a6bf42c0dc154fafbb9ddf07873
hyperexe805 <sendmemes36@gmail.com>
2024-11-28 00:00:40 +0100

    initial app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Fragment } from 'react/jsx-runtime'
+import './App.css'
+
+function App() {
+
+  return (
+    <Fragment>
+      <main>
+        <p className=''>Welcome</p>
+      </main>
+    </Fragment>
+  )
+}
+
+// 1:12:52
+export default App
